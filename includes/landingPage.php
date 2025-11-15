b5902a0bce0260d281b2676495e63bc69ad1fc1a
E-Was24 <satriaelan5@gmail.com>
2025-11-16 00:59:32 +0700

    deploy fix working

--- includes/landingPage.php
+++ includes/landingPage.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Landing page</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../assets/css/landingPage.css">
 </head>
 
 <body>
@@ -52,7 +52,7 @@
             </div>
         </div>
     </section> -->
-    
+
 </body>
 
 </html>
