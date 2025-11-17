93ce49dd5e26045ea05283222d6a793a7adfdfeb
E-Was24 <satriaelan5@gmail.com>
2025-11-17 21:07:06 +0700

    update

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <title><?= $app['title'] ?> - Welcome</title>
     <link rel="stylesheet" href="assets/css/styleV1.css">
     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
-    <link rel="stylesheet" href="assets/css/navbarV1.0.1.css">
+    <link rel="stylesheet" href="assets/css/navbarV1.0.1.0.css">
     <link rel="stylesheet" href="assets/css/landingPageV1.css">
     <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
 </head>
