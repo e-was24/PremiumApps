a4c978346435ed0edba2679926e383028924f71e
E-Was24 <satriaelan5@gmail.com>
2025-11-17 21:42:46 +0700

    up

--- index.php
+++ index.php
@@ -5,10 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $app['title'] ?> - Welcome</title>
-    <link rel="stylesheet" href="assets/css/tes.css">
+    <link rel="stylesheet" href="assets/css/styleV1.0.css">
     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
     <link rel="stylesheet" href="assets/css/navbarV1.0.1.0.css">
-    <link rel="stylesheet" href="assets/css/landingPageV1.0.css">
+    <!-- <link rel="stylesheet" href="assets/css/landingPageV1.0.css"> -->
+    <link rel="stylesheet" href="assets/css/tes.css">
     <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
 </head>
 
