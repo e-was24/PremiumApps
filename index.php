8f7c43368cc948ce6ed06723dde8ad69202c83a2
E-Was24 <satriaelan5@gmail.com>
2025-11-17 22:26:51 +0700

    upnavbar

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
     <title><?= $app['title'] ?> - Welcome</title>
     <link rel="stylesheet" href="assets/css/styleV1.0.css">
     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
-    <link rel="stylesheet" href="assets/css/navbarV1.0.1.0.css">
-    <link rel="stylesheet" href="assets/css/landingPageV1.0.1.css">
+    <link rel="stylesheet" href="assets/css/navbarV1.0.1.1.css">
+    <link rel="stylesheet" href="assets/css/landingPageV1.0.11.css">
     <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
 </head>
 
