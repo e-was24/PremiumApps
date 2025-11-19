cd251038fcbdc85f2c7cd3954d28fe712c5228dc
E-Was24 <satriaelan5@gmail.com>
2025-11-19 20:24:54 +0700

    v 0.0.0.1.6 (deploy)

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
 
     <!-- COMPONENTS -->
-    <link rel="stylesheet" href="assets/css/navbarV1.0.28.css">
+    <link rel="stylesheet" href="assets/css/navbarV1.0.29.css">
     <link rel="stylesheet" href="assets/css/landingPageV1.0.20.css">
 </head>
 
