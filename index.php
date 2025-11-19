61362c63952c75ffc1a21596f1b34daa73268e13
E-Was24 <satriaelan5@gmail.com>
2025-11-19 15:02:13 +0700

    v 0.0.0.0.6 (deploy)

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <title><?= htmlspecialchars($app['title'] ?? 'Premium Apps') ?> | Welcome</title>
 
     <!-- MAIN CSS -->
-    <link rel="stylesheet" href="assets/css/styleV1.0.css">
+    <link rel="stylesheet" href="assets/css/styleV1.0.2.css">
 
     <!-- AOS ANIMATION -->
     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
