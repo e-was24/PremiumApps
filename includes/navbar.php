2596cd7bf95d1dee6e943cb9c59756dc4710d9af
E-Was24 <satriaelan5@gmail.com>
2025-11-19 16:19:09 +0700

    v 0.0.0.1.3 (deploy)

--- includes/navbar.php
+++ includes/navbar.php
@@ -4,8 +4,10 @@
     </svg>
 </button>
 
-<nav class="nav-top" id="navbar">
+<!-- GANTI nav-top → hapus saja -->
+<nav id="navbar">
     <div class="logo"><span>Premium</span><br>Apps .</div>
+
     <ul>
         <li><a href="#">
                 <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
@@ -15,6 +17,7 @@
                 </svg>
                 Home
             </a></li>
+
         <li><a href="#">
                 <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
                     fill="#e3e3e3">
@@ -23,14 +26,16 @@
                 </svg>
                 Transaction
             </a></li>
+
         <li><a href="#">
                 <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
                     fill="#e3e3e3">
                     <path
                         d="M240-80q-50 0-85-35t-35-85v-120h120v-560h600v680q0 50-35 85t-85 35H240Zm480-80q17 0 28.5-11.5T760-200v-600H320v480h360v120q0 17 11.5 28.5T720-160ZM360-600v-80h360v80H360Zm0 120v-80h360v80H360ZM240-160h360v-80H200v40q0 17 11.5 28.5T240-160Zm0 0h-40 400-360Z" />
                 </svg>
-                price
+                Price
             </a></li>
+
         <li><a href="#">
                 <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
                     fill="#e3e3e3">
