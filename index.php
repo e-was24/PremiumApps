772bdf8cb395ddc3bc3bff91a2420ed766b1afc7
E-Was24 <satriaelan5@gmail.com>
2025-11-19 17:12:08 +0700

    css fix (fvk)

--- index.php
+++ index.php
@@ -14,8 +14,8 @@
     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
 
     <!-- COMPONENTS -->
-    <link rel="stylesheet" href="assets/css/navbarV1.0.20.css">
-    <link rel="stylesheet" href="assets/css/landingPageV1.0.13.css">
+    <link rel="stylesheet" href="assets/css/navbarV1.0.21.css">
+    <link rel="stylesheet" href="assets/css/landingPageV1.0.14.css">
 </head>
 
 <body>
